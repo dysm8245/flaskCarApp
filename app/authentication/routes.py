24d5613403d170f80ed384fc24adccca5b90ea3c
Dylan Smith <dysm8245@colorado.edu>
2023-10-16 20:52:13 -0600

    edited backend auth routes for react

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from forms import UserSignupForm, UserLoginForm
 from models import User, db, check_password_hash
 from flask_login import login_user, logout_user, LoginManager, current_user, login_required
@@ -8,15 +8,13 @@ auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = UserSignupForm()
-
     try:
-        if request.method == 'POST' and form.validate_on_submit():
-            first_name = form.first_name.data
-            last_name = form.last_name.data
-            email = form.email.data
-            username = form.username.data
-            password = form.password.data
+        if request.method == 'POST':
+            first_name = request.json['first']
+            last_name = request.json['last']
+            email = request.json['email']
+            username = request.json['username']
+            password = request.json['password']
 
             user = User(first_name, last_name, email, username, password)
 
@@ -25,34 +23,40 @@ def signup():
 
             print('successfully added user')
             
-            return redirect(url_for('site.profile'))
+            return jsonify({
+                "token": user.token
+            })
         else:
-            print("couldn't add user")
+            return jsonify({
+                "error": "Couldn't create account"
+            }, 401)
     except:
         raise Exception('Invalid form data: Please check your form')
     
-    return render_template('signup.html', form = form)
+    # return render_template('signup.html')
 
 @auth.route('/signin', methods=['GET', 'POST'])
 def signin():
-    form = UserLoginForm()
     try:
-        if request.method == 'POST' and form.validate_on_submit():
-            print('valid')
-            username = form.username.data
-            password = form.password.data
+        if request.method == 'POST':
+            username = request.json["username"]
+            password = request.json["password"]
 
             logged_user = User.query.filter(User.username == username).first()
             if logged_user and check_password_hash(logged_user.password, password):
-                login_user(logged_user)
-                print('logged in')
-                return redirect(url_for('site.profile'))
+                # login_user(logged_user)
+                # print('logged in')
+                return jsonify({
+                    "token": logged_user.token
+                }) 
             else:
-                print('We were not able to log in this user')
+                return jsonify({
+                    "error": "No user in Database"
+                }, 401)
     except:
         raise Exception('Invalid form data: Please check your form')
     
-    return render_template('signin.html', form = form)
+    # return render_template('signin.html')
 
 @auth.route('/logout')
 def logout():
